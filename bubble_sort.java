51584489262f574b56a9e3a469fb8d23b7a9e3d5
shruti <122924152+shruti-ugale-code@users.noreply.github.com>
2024-07-08 21:53:30 +0530

    Add files via upload

---
+++ bubble_sort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class bubble_sort {
+    public static void main(String[] args) {
+        int arr[]={10,4,3,6,5,7,100,99};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    public static void bubble(int arr[]){
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 0; j <arr.length-i ; j++) {
+                if(arr[j-1]> arr[j]){
+                   int temp= arr[j];
+                   arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+
+            }
+
+        }
+    }
+}

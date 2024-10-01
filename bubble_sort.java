e510631b78f959c6a2617ffb3fc55512855596d3
shruti <122924152+shruti-ugale-code@users.noreply.github.com>
2024-10-01 11:43:00 +0530

    Update bubble_sort.java

--- bubble_sort.java
+++ bubble_sort.java
@@ -9,7 +9,7 @@ public class bubble_sort {
     }
     public static void bubble(int arr[]){
         for (int i = 0; i < arr.length ; i++) {
-            for (int j = 0; j <arr.length-i ; j++) {
+            for (int j = 1; j <arr.length-i ; j++) {
                 if(arr[j-1]> arr[j]){
                    int temp= arr[j];
                    arr[j]=arr[j-1];
